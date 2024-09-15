930178269093cda13570865a867f03ddf301c22e
David Singletary <38020984+ProfSingletary@users.noreply.github.com>
2024-09-15 12:10:12 -0400

    Add files via upload

---
+++ cop2073c-pe4Solve.R
@@ -0,0 +1,17 @@
+# cop2073c-pe4Solve
+# D. Singletary
+# 9/15/24
+# plots height vs. weight using qplot, geoms, and aesthetic mapping
+
+# create weight and height vectors
+w <- c(55,85,75,42,93,63,58,75,89,67)
+h <- c(161,185,174,154,188,178,170,167,181,178)
+
+# set levels for sex as factor
+s <- factor(
+         c("female","male","male","female","male",
+           "male","female","male","male","female"))
+
+# let qplot do its thing
+qplot(w, h, color=s, shape=s, xlab="Weight (kg)", ylab="Height (cm)",
+      main="Height vs. weight for 10 people") + geom_point(size=4)
